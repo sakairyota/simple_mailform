19e31a8c4dc2be76664557915dc11aec7505ccb6
sakairyota <sakai@cloudstudy.com>
2012-03-06 17:22:23 +0900

    add feature  reply mail

--- app/index.php
+++ app/index.php
@@ -51,6 +51,36 @@ $settings['subject'] = 'Simple Mailform';
 //
 $settings['error_not_exist_required_parameter'] = '%s is required.';
 
+// reply_mail (optional)
+//     enable reply mail for sender.
+//
+$settings['replyMail'] = false;
+
+// reply_mail_param_name (optional)
+//     defalut: 'email'
+//
+//$settings['replyMailParamName'] = 'email';
+
+// reply_subject (optional)
+//     default: $settings['subject']
+//
+//$settings['replySubject'] = 'Thank you!';
+
+// reply_from (optional)
+//     default: $settings['from']
+//
+//$settings['replyFrom'] = 'info@example.com';
+
+// reply_from (required)
+//     (optional if replyMail is false)
+//     
+//     replace [name] into name parameter
+//
+/*$settings['replyFormat'] = <<<END_FORMAT
+thank you!
+
+your email address is {email}.
+END_FORMAT;*/
 
 // Mailform Script //
 
@@ -78,6 +108,18 @@ if ($_POST){
 	if (!isset($settings['completeHtmlName'])) {
 		$settings['completeHtmlName'] = 'complete.html';
 	}
+	if (!isset($settings['replyMailParamName'])) {
+		$settings['replyMailParamName'] = 'email';
+	}
+	if (!isset($settings['replySubject'])) {
+		$settings['replySubject'] = $settings['subject'];
+	}
+	if (!isset($settings['replyFrom'])) {
+		$settings['replyFrom'] = $settings['from'];
+	}
+	if (!isset($settings['replyFormat']) && $settings['replyMail']) {
+		$errors[] = "[Settings] 'replyFormat' setting is REQUIRED";
+	}
 	
 	// form parameter variable
 	$parameters = $settings['parameters'];
@@ -89,6 +131,13 @@ if ($_POST){
 			$parameters[$name]['formalName'] = $name;
 		}
 	}
+
+  // validate replyMailParamName parameter
+	if ($settings['replyMail']) {
+    if (! isset($parameters[$settings['replyMailParamName']])){
+		  $errors[] = "[Settings] replyMailParamName does not exist in the parameter.";
+    }
+  }
 
 	// validate required parameters
   foreach($parameters as $name => $parameter){
@@ -120,13 +169,31 @@ if ($_POST){
 	}
 
 	// send mail if not exist errors
-	if (!$errors){
+	if (!$errors) {
 
 		//send mail
 		$result = mb_send_mail($to, $subject, $body, $headers);
 
 		// redirect to complete page if succeed.
-		if ($result){
+		if ($result) {
+
+      //send reply mail
+      if($settings['replyMail']) {
+	      $replay_from = $settings['replyFrom'];
+	      $reply_to = $_POST[$settings['replyMailParamName']];
+	      $reply_subject = $settings['replySubject'];
+	      $reply_headers = '';
+	      $reply_body = $settings['replyFormat'];
+
+	      $reply_headers .= "From: <{$from}>";
+
+        // replace [name] to actual parameter
+        foreach($_POST as $name => $param) {
+          $reply_body = ereg_replace("\{{$name}\}", $param, $reply_body);
+        }
+
+		    $result = mb_send_mail($reply_to, $reply_subject, $reply_body, $reply_headers);
+      }
 
 			//construct URL of complete page.
 			$completeHtmlUrl  = 'http://';
